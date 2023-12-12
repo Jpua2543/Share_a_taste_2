47ee703b247995674150365ead1b3b9ef5499703
John <johnpua@outlook.com>
2023-12-11 16:15:50 -0800

    adding edit to comment

--- flask_app/controllers/comments.py
+++ flask_app/controllers/comments.py
@@ -18,3 +18,43 @@ def add_comment():
     }
     Comment.save(data)
     return redirect(f"/comments/{request.form['recipe_id']}")
+
+@app.route("/recipes/add_comment/<int:recipe_id>", methods=["POST"])
+def add_comment(recipe_id):
+    if 'user_id' not in session:
+        flash('You must be logged in to comment')
+        return redirect('/login')
+
+    data = {
+        "user_id": session['user_id'],
+        "recipe_id": recipe_id,
+        "comment": request.form['comment'],
+        "name": request.form['name']
+    }
+
+    Comment.save(data)
+    return redirect(f"/recipes/view/{recipe_id}")
+
+@app.route("/comments/edit/<int:comment_id>", methods=["GET", "POST"])
+def edit_comment(comment_id):
+    if 'user_id' not in session:
+        flash("Please log in to edit comments.", "error")
+        return redirect('/login')
+    comment = Comment.get_by_id(comment_id)
+    if comment and session['user_id'] == comment.user_id:
+        if request.method == "POST":
+            if Comment.validate_comment(request.form):
+                updated_comment = {
+                    "comment_id": comment_id,
+                    "comment": request.form['comment']
+                }
+                Comment.update(updated_comment)
+                flash("Comment updated successfully.", "success")
+                return redirect(f"/recipes/view/{comment.recipe_id}")
+            else:
+                return redirect(request.url)
+        else:
+            return render_template("edit_comment.html", comment=comment)
+    else:
+        flash("Comment not found or you do not have permission to edit this comment.", "error")
+        return redirect('/dashboard')
--- flask_app/models/comment.py
+++ flask_app/models/comment.py
@@ -9,8 +9,8 @@ class Comment:
 
     def __init__(self, data):
         self.id = data['id']
-        self.user_id = data['user_id']
-        self.recipe_id = data['recipe_id']
+        self.User_id = data['User_id']
+        self.Recipe_id = data['Recipe_id']
         self.comment = data['comment']
         self.name = data['name']
         self.created_at = data['created_at']
@@ -18,8 +18,8 @@ class Comment:
     @classmethod
     def save(cls, data):
         query = """
-            INSERT INTO comments (user_id, recipe_id, comment, name, created_at)
-            VALUES (%(user_id)s, %(recipe_id)s, %(comment)s, %(name)s, NOW());
+            INSERT INTO comments (User_id, Recipe_id, comment, name, created_at)
+            VALUES (%(User_id)s, %(Recipe_id)s, %(comment)s, %(name)s, NOW());
         """
         result = connectToMySQL(cls.DB).query_db(query, data)
         return result
@@ -33,3 +33,29 @@ class Comment:
         for result in results:
             comments.append(cls(result))
         return comments
+    
+    @classmethod
+    def validate_comment(comment_data):
+        is_valid = True
+        if len(comment_data['comment'].strip()) == 0:
+            is_valid = False
+            flash("Comment cannot be empty.")
+        return is_valid
+
+    @classmethod
+    def get_by_id(cls, comment_id):
+        query = "SELECT * FROM comments WHERE id = %(comment_id)s;"
+        data = {'comment_id': comment_id}
+        result = connectToMySQL(cls.DB).query_db(query, data)
+        if result:
+            return cls(result[0])
+        return None
+
+    @classmethod
+    def update(cls, data):
+        query = """
+            UPDATE comments
+            SET comment = %(comment)s
+            WHERE id = %(comment_id)s;
+        """
+        return connectToMySQL(cls.DB).query_db(query, data)
